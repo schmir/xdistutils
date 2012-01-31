1f575842e8a2993a0f1275a5c0fc46c93271deb3
Ralf Schmitt <ralf@systemexit.de>
2012-01-31 01:02:31 +0100

    get rid of classifiers, fix url and add long description to setup.py

--- setup.py
+++ setup.py
@@ -4,24 +4,9 @@ from distutils.core import setup
 
 setup(name="xdistutils",
       description="provide useful distutils commands",
+      long_description=open("README.rst").read(),
       version="0.1.0",
       packages=["xdistutils"],
       maintainer="Ralf Schmitt",
       maintainer_email="ralf@systemexit.de",
-      url="https://github.com/schmir/pypiserver",
-      classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-       "License :: OSI Approved :: zlib/libpng License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.0",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: System :: Software Distribution"])
+      url="https://github.com/schmir/xdistutils")
