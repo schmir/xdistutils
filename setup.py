99d3b89527b7fc20c89de6b3d4c76c978847b446
Ralf Schmitt <ralf@systemexit.de>
2012-02-08 20:25:54 +0100

    up version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name="xdistutils",
       description="provide useful distutils commands",
       long_description=open("README.rst").read(),
-      version="0.1.0",
+      version="0.2.0",
       packages=["xdistutils"],
       maintainer="Ralf Schmitt",
       maintainer_email="ralf@systemexit.de",
